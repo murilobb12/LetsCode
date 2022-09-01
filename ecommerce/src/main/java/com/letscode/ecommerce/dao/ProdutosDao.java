fdb2d256556f611c9864f98f68e0f4ae11b4a64c
f7287356 <murilo.asantos@bb.com.br>
2022-09-01 16:05:24 -0300

    Adicionando comentários

--- ecommerce/src/main/java/com/letscode/ecommerce/dao/ProdutosDao.java
+++ ecommerce/src/main/java/com/letscode/ecommerce/dao/ProdutosDao.java
@@ -1,11 +1,27 @@
-package com.letscode.ecommerce.dao;
+    package com.letscode.ecommerce.dao;
 
 import com.letscode.ecommerce.models.Cliente;
 import com.letscode.ecommerce.models.Produtos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProdutosDao extends JpaRepository<Produtos, Long> {
 
+    //Named Methods
     Produtos findAllById(long id);
+//
+//    //Jpa Native Query
+//    @Query(value = "SELECT * FROM Produtos WHERE id = userId", nativeQuery = true)
+//    Produtos findByProductId(long userId);
+
+    //JPQL
+//    @Query("SELECT * FROM Produtos WHERE id = :id")
+//    Produtos findProductByID(@Param("id") long id);
+
+
+
 
 }
