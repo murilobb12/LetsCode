0e6b4da988f2f477026fb53f104e3ce74d457a67
F7287356 <murilo.asantos@bb.com.br>
2022-10-18 15:38:11 -0300

    Removendo pasta de code-with-quarkus

--- Quarkus/code-with-quarkus/src/main/resources/import.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO Fruta(id, nome, qtd) VALUES (1, 'TESTE', 5);
--- Quarkus/code-with-quarkus/src/test/java/br/com/murilo/quarkus/FrutaResourceIT.java
+++
@@ -1,8 +0,0 @@
-package br.com.murilo.quarkus;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class FrutaResourceIT extends FrutaResourceTest {
-    // Execute the same tests but in packaged mode.
-}
