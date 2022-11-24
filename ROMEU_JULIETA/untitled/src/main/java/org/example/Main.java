0d17bf0e6a1219fc86f2d31cae283367211baa43
F7287356 <murilo.asantos@bb.com.br>
2022-11-24 13:13:38 -0300

    Adicionar Projeto Mentoria

---
+++ ROMEU_JULIETA/untitled/src/main/java/org/example/Main.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import sun.util.locale.LocaleUtils;
+
+import static java.lang.Character.isDigit;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(romeu_julieta(1));
+
+    }
+
+    public static int romeu_julieta(int valor){
+
+            if (Integer.)
+        }
+            return 0;
+
+    }
+
+    public static boolean tratarEntrada(Object valor) {
+
+        String nome = "Murilo";
+
+        boolean murilo = nome.equals("Murilo");
+
+        return murilo;
+l
+    }
+
+}
