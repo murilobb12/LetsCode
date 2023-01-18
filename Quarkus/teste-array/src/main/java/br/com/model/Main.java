8aa01527a98a80b900687c14fb89ff6d105539de
F7287356 <murilo.asantos@bb.com.br>
2023-01-18 16:09:38 -0300

    Adicionando validações no teste com array

--- Quarkus/teste-array/src/main/java/br/com/model/Main.java
+++ Quarkus/teste-array/src/main/java/br/com/model/Main.java
@@ -6,28 +6,30 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /*Entrada entrada = new Entrada();
+//        /*Entrada entrada = new Entrada();
+//
+//        entrada.setClientes(new Cliente[]{new Cliente("Murilo", 27, new Filho[]{new Filho("Cleber", 1)})});*/
+//
+//        ArrayList<Filho> filhos = new ArrayList<>();
+//        ArrayList<Cliente> clientes = new ArrayList<>();
+//        Entrada entrada = new Entrada();
+//        Cliente cliente = new Cliente();
+//        Filho filho = new Filho("Cleber", 1);
+//        filhos.add(filho);
+//        cliente.setNome("Murilo");
+//        cliente.setIdade(27);
+//        cliente.setFilhos(filhos);
+//        clientes.add(cliente);
+//
+//        System.out.println(clientes);
+//
+//
+//        System.out.println(entrada);
 
-        entrada.setClientes(new Cliente[]{new Cliente("Murilo", 27, new Filho[]{new Filho("Cleber", 1)})});*/
 
-        ArrayList<Filho> filhos = new ArrayList<>();
-        ArrayList<Cliente> clientes = new ArrayList<>();
         Entrada entrada = new Entrada();
-        Cliente cliente = new Cliente();
-        Filho filho = new Filho("Cleber", 1);
-        filhos.add(filho);
-        cliente.setNome("Murilo");
-        cliente.setIdade(27);
-        cliente.setFilhos(filhos);
-        clientes.add(cliente);
-
-        System.out.println(clientes);
-
 
         System.out.println(entrada);
-
-
-
 
 
 
--- Quarkus/teste-array/src/main/java/br/com/rest/ClienteResource.java
+++ Quarkus/teste-array/src/main/java/br/com/rest/ClienteResource.java
@@ -3,25 +3,60 @@ package br.com.rest;
 
 import br.com.model.Cliente;
 import br.com.model.Entrada;
+import br.com.repository.ClienteRepository;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("/clientes")
 @RequestScoped
-public class ClienteResource
-{
+public class ClienteResource {
 
+    @Inject
+    ClienteRepository clienteRepository;
+
+
+
+    @Inject
+    Entrada entrada;
+
+    @Path("/entrada")
     @POST
-    public Response inserirCliente(Entrada entrada){
+    public Response inserirCliente(Entrada jsonEntrada) {
 
+        entrada.setClientes(jsonEntrada.clientes);
+
+        System.out.println(entrada);
 
         return Response.status(Response.Status.OK.getStatusCode()).entity(entrada).build();
 
 
+    }
+    @Path("/array")
+    @POST
+    public Response inserirCliente( ) {
 
+
+        System.out.println(entrada.getClientes()[2]);
+//        Cliente cliente = entrada.clientes[1];
+//        cliente.setNome(jsonEntrada.getNome());
+//        cliente.setIdade(jsonEntrada.getIdade());
+//        cliente.setFilhos(jsonEntrada.getFilhos());
+
+        return Response.status(Response.Status.OK.getStatusCode()).build();
+
+
+    }
+
+
+    @GET
+    public Cliente listarCliente(){
+        return entrada.getClientes()[1];
     }
 
 }
