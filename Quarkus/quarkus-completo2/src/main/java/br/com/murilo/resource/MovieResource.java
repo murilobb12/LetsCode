a42b78da1eead28d6fb435742f845eb400f02d7c
F7287356 <murilo.asantos@bb.com.br>
2023-02-01 16:39:19 -0300

    Iniciando estudos Quarkus Microprofile

--- Quarkus/quarkus-completo2/src/main/java/br/com/murilo/resource/MovieResource.java
+++ Quarkus/quarkus-completo2/src/main/java/br/com/murilo/resource/MovieResource.java
@@ -1,18 +1,23 @@
 package br.com.murilo.resource;
 
 import br.com.murilo.model.Movie;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 
+@Tag(name = "Movie Resource", description = "Movie REST APIs")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 @Path("/movies")
@@ -20,6 +25,14 @@ public class MovieResource {
 
     List<Movie> movieList = new ArrayList<>();
 
+    @Operation(operationId = "getMovies",
+            summary = "Get Movies",
+            description = "Get all movies inside the list")
+    @APIResponse(
+            responseCode = "200",
+            description = "Operation Completed",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON)
+    )
     @GET
     public Response listMovies() {
 
@@ -27,15 +40,36 @@ public class MovieResource {
 
     }
 
+    @Operation(
+            operationId = "insertMovie",
+            summary = "Insert Movie",
+            description = "Insert movie inside the list"
+    )
+    @APIResponse(
+            responseCode = "201",
+            description = "Operation Completed",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON)
+    )
     @POST
-    public Response insertMovie(Movie newMovie) {
-
+    public Response insertMovie(
+            @RequestBody(
+                    description = "Movie to create",
+                    required = true,
+                    content = @Content(schema = @Schema(implementation = Movie.class))
+            )
+            Movie newMovie) {
         movieList.add(newMovie);
-
         return Response.status(Response.Status.OK.getStatusCode()).build();
-
     }
 
+    @Operation(operationId = "countMovies",
+            summary = "Count Movies",
+            description = "Size of movie list")
+    @APIResponse(
+            responseCode = "200",
+            description = "Operation Completed",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON)
+    )
     @GET
     @Path("/count")
     public Response countMovies() {
@@ -47,7 +81,6 @@ public class MovieResource {
 
     @PUT
     public Response updateMovie(Movie updatedMovie) {
-
 
         movieList = movieList.stream().peek(movie -> {
             if (movie.getId().equals(updatedMovie.getId())) {
@@ -69,8 +102,8 @@ public class MovieResource {
 
         boolean removed = false;
 
-        if (movieToDelete.isPresent()){
-             removed = movieList.remove(movieToDelete.get());
+        if (movieToDelete.isPresent()) {
+            removed = movieList.remove(movieToDelete.get());
         }
 
 
